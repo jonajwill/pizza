bb5c2fa6aca30ba2236047b9337168097cea90ed
jonajwill <jona.will@studium.uni-hamburg.de>
2026-06-20 17:19:52 +0200

    zutatenRechner so eine Art fertig. leere Zutaten werden nicht angegeben. Man könnte noch an der Reihenfolge arbeiten oder hinzufügen, dass er ausgiebt, welche Pizzen das macht

--- zutatenRechner.py
+++ zutatenRechner.py
@@ -1,3 +1,19 @@
+marinara=0
+margherita=2
+funghi=1
+salame=1
+funghiesalame=0
+caprese=1
+cacioepepe=0
+verdura=0
+quattrostagioni=0
+rossa=2
+parmigiana=1
+caprino=1
+stracciatella=1
+cipolle=0
+
+
 def zutaten(marinara, margherita, funghi, salame, funghiesalame, caprese, cacioepepe, verdura, quattrostagioni, rossa, parmigiana, caprino, stracciatella, cipolle):
     tomaten=100*(marinara+margherita+funghi+salame+funghiesalame+caprese+verdura+quattrostagioni+parmigiana+caprino+stracciatella)
     fiordilatte=90*(margherita+funghi+salame+funghiesalame+caprese+verdura+quattrostagioni+parmigiana+caprino)
@@ -24,6 +40,12 @@ def zutaten(marinara, margherita, funghi, salame, funghiesalame, caprese, cacioe
     stracciatellaDiBurrata=stracciatella
     ricotta=2*cipolle
     zwiebeln=cipolle
-    return print(tomaten, "g San Marzano Tomaten\n",fiordilatte, "g Fior di latte\n", basilikum, "Blätter Basilikum\n", olivenöl, "Schuss Olivenöl", parmigiano, "g Parmigiano Reggiano\n", champignons, "Champignons\n", knoblauch, "Knoblauchzehen\n", oregano, "Prisen Oregano\n", salami, "Scheiben Salami\n", kirschtomaten, "Kirschtomaten\n", basilikumpesto, "EL Basilikumpesto\n", balsamico, "Schuss Aceto di balsamico\n", pecorino, "g Pecorino Romano\n", pfeffer, "schwarze Pfefferkörner\n", zucchini, "Zucchinischeiben\n", paprika, "rote Paprika\n", getrockneteTomaten, "getrocknete Tomaten\n", pestoRosso, "g Pesto Rosso", burrata, "Burrata\n", pinienkerne, "EL Pinienkerne\n", aubergine, "Scheiben Aubergine\n", ziegenkäse, "Scheiben Ziegenkäse\n", stracciatellaDiBurrata, "Portionen Stracciatella di burrata\n",ricotta, "EL Ricotta\n", zwiebeln, "Portionen karamellisierte Zwiebeln")
+    zutatenliste=[tomaten, "g San Marzano Tomaten\n",fiordilatte, "g Fior di latte\n", basilikum, "Blätter Basilikum\n", parmigiano, "g Parmigiano Reggiano\n", champignons, "Champignons\n", knoblauch, "Knoblauchzehen\n", oregano, "Prisen Oregano\n", salami, "Scheiben vegetarische Salami\n", kirschtomaten, "Kirschtomaten\n", basilikumpesto, "EL Basilikumpesto\n", balsamico, "Schuss Aceto di balsamico\n", pecorino, "g Pecorino Romano\n", pfeffer, "schwarze Pfefferkörner\n", zucchini, "Zucchinischeiben\n", paprika, "rote Paprika\n", pestoRosso, "g Pesto Rosso\n", getrockneteTomaten, "getrocknete Tomaten\n", burrata, "Burrata\n", pinienkerne, "EL Pinienkerne\n", aubergine, "Scheiben Aubergine\n", ziegenkäse, "Scheiben Ziegenkäse\n", stracciatellaDiBurrata, "Portionen Stracciatella di burrata\n",ricotta, "EL Ricotta\n", zwiebeln, "Portionen karamellisierte Zwiebeln", olivenöl, "Schuss Olivenöl\n"]
+    ausgabe="Zutaten:\n"
+    for i in range(int(len(zutatenliste)/2)):
+        if zutatenliste[2*i]!=0:
+            ausgabe+=str(zutatenliste[2*i])+' '+str(zutatenliste[2*i+1])
+    return print(ausgabe)
 
-zutaten(2,1,0,0,0,0,0,0,0,0,0,0,0,0)
+# zutaten(2,1,0,0,0,0,0,0,0,0,0,0,0,0)
+zutaten(marinara, margherita, funghi, salame, funghiesalame, caprese, cacioepepe, verdura, quattrostagioni, rossa, parmigiana, caprino, stracciatella, cipolle)
